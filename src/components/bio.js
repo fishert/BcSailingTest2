165037a9aa82b457c5eac9032316dbe65bf619d0
fishert <ted.gfish@gmail.com>
2019-10-11 19:11:44 -0600

    I changed a couple things (author)

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,11 +54,10 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        Written by <strong>{author}</strong> who live and work in Montana building useful things.
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+          Twitter anyone?
         </a>
       </p>
     </div>
